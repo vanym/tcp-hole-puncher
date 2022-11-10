536afaa1cf196d859223f59a46de14b5599f38e2
VanyM <vanymdev@gmail.com>
2022-11-10 09:58:10 +0400

    change app to make multiple ws connections and multiple stun request

--- cli_args.go
+++ cli_args.go
@@ -4,27 +4,57 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
+type argStringSlice []string
+
+func (a *argStringSlice) Set(s string) error {
+	*a = append(*a, s)
+	return nil
+}
+
+func (a *argStringSlice) String() string {
+	return strings.Join(*a, ",")
+}
+
 type appOptions struct {
-	webSocketEchoURL string
-	stunAddress      string
-	bindAddress      string
-	trace            bool
+	webSocketEchoURLs argStringSlice
+	stunAddresses     argStringSlice
+	bindAddress       string
+	trace             bool
 }
 
 var flagSet *flag.FlagSet
 
 const programName string = "tcp-hole-puncher"
-const version string = "0.1.4"
+const version string = "0.2.0"
+
+var defaultBind = ":7203"
+var defaultWsUrls = [...]string{
+	"wss://ws.vi-server.org/mirror",
+	"wss://ws.postman-echo.com/raw",
+	"wss://ws.ifelse.io/",
+	"wss://echo.websocket.events/",
+}
+var defaultStuns = [...]string{
+	"stun.sipnet.net:3478",
+	"stun.antisip.com:3478",
+	"stun.voipgate.com:3478",
+	"stun.nextcloud.com:443",
+	"stun.ipfire.org:3478",
+	"stun.freeswitch.org:3478",
+	"stun.sonetel.net:3478",
+}
 
 func parseCliArgs() (args appOptions) {
 	flagSet = flag.NewFlagSet(programName, flag.ContinueOnError)
 	flagSet.BoolVar(&args.trace, "trace", false, "Trace output")
 
-	flagSet.StringVar(&args.webSocketEchoURL, "ws-url", "wss://ws.vi-server.org/mirror", "Websocket echo server url")
-	flagSet.StringVar(&args.bindAddress, "bind", ":7203", "Bind address")
-	flagSet.StringVar(&args.stunAddress, "stun", "stun.sipnet.net:3478", "Stun server address")
+	flagSet.StringVar(&args.bindAddress, "bind", defaultBind, "Bind address")
+
+	flagSet.Var(&args.webSocketEchoURLs, "ws-url", "Websocket echo server url (default servers if none provided)\n    \""+strings.Join(defaultWsUrls[:], "\"\n    \"")+"\"")
+	flagSet.Var(&args.stunAddresses, "stun", "Stun server address (default servers if none provided)\n    \""+strings.Join(defaultStuns[:], "\"\n    \"")+"\"")
 
 	pVersion := flagSet.Bool("version", false, "Print version")
 
--- main.go
+++ main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"io"
 	"log"
 	"net"
+	"os"
+	"sort"
 	"strconv"
+	"strings"
+	"sync"
+	"sync/atomic"
 	"time"
 
 	"golang.org/x/net/websocket"
@@ -18,16 +24,22 @@ import (
 
 func main() {
 	args := parseCliArgs()
+	if len(args.webSocketEchoURLs) == 0 {
+		args.webSocketEchoURLs = defaultWsUrls[:]
+	}
+	if len(args.stunAddresses) == 0 {
+		args.stunAddresses = defaultStuns[:]
+	}
 	if !args.trace {
 		log.SetOutput(io.Discard)
 	}
 	if !reuseport.Available() {
-		log.Println("Port reuse not available")
+		fmt.Fprintln(os.Stderr, "Port reuse not available")
 		return
 	}
 	bindAddr, err := net.ResolveTCPAddr("tcp", args.bindAddress)
 	if err != nil {
-		log.Println("Can't resolve bind address", args.bindAddress, err)
+		fmt.Fprintln(os.Stderr, "Can't resolve bind address", args.bindAddress, err)
 		return
 	}
 	dialer := &net.Dialer{
@@ -35,14 +47,52 @@ func main() {
 		LocalAddr: bindAddr,
 	}
 	ctx := context.Background()
+	nurls := len(args.webSocketEchoURLs)
+	connectTimeCh := make(chan time.Time, nurls*16)
+	writeTimeCh := make(chan time.Time)
+	for _, webSocketEchoURL := range args.webSocketEchoURLs {
+		go webSocketLoop(ctx, dialer, webSocketEchoURL, connectTimeCh, writeTimeCh)
+	}
+	stuns := make(stunsHolder, len(args.stunAddresses))
+	copy(stuns, args.stunAddresses)
+	var lastTime time.Time
+	var stunerr error
 	tick := time.Tick(time.Second)
 	for {
-		runConnection(ctx, dialer, args.webSocketEchoURL, args.stunAddress)
+		if stunerr == nil {
+			conTime := <-connectTimeCh
+			if !conTime.After(lastTime) {
+				continue
+			}
+			time.Sleep(time.Second)
+		} else {
+			select {
+			case <-writeTimeCh:
+			case <-connectTimeCh:
+			}
+			<-tick
+		}
+		var addr net.Addr
+		var checkTime time.Time
+		addr, checkTime, stunerr = stuns.getAddress(ctx, dialer)
+		if stunerr != nil {
+			log.Println("Can't get address", stunerr)
+			continue
+		}
+		lastTime = checkTime
+		fmt.Println(addr)
+	}
+}
+
+func webSocketLoop(ctx context.Context, dialer *net.Dialer, webSocketEchoURL string, connectTimeCh chan<- time.Time, writeTimeCh chan<- time.Time) {
+	tick := time.Tick(4 * time.Second)
+	for {
+		runConnection(ctx, dialer, webSocketEchoURL, connectTimeCh, writeTimeCh)
 		<-tick
 	}
 }
 
-func runConnection(ctx context.Context, dialer *net.Dialer, webSocketEchoURL string, stunAddress string) {
+func runConnection(ctx context.Context, dialer *net.Dialer, webSocketEchoURL string, connectTimeCh chan<- time.Time, writeTimeCh chan<- time.Time) {
 	myCtx, cancel := context.WithCancel(ctx)
 	defer cancel()
 	config, err := websocket.NewConfig(webSocketEchoURL, webSocketEchoURL)
@@ -51,28 +101,18 @@ func runConnection(ctx context.Context, dialer *net.Dialer, webSocketEchoURL str
 		return
 	}
 	config.Dialer = dialer
-	log.Println("Connecting to websocket")
+	log.Println("Connecting to websocket", webSocketEchoURL)
 	conn, err := websocket.DialConfig(config)
 	if err != nil {
-		log.Println("Can't connect websocket", err)
+		log.Println("Can't connect websocket "+webSocketEchoURL, err)
 		return
 	}
 	defer conn.Close()
+	connectTimeCh <- time.Now()
+	log.Println("Connected to websocket", webSocketEchoURL)
 	go io.Copy(io.Discard, conn)
-	var stunerr error = io.ErrNoProgress
 	tick := time.Tick(time.Second * 5)
 	for {
-		if stunerr != nil {
-			stunCtx, stunCtxCancel := context.WithTimeout(myCtx, 10*time.Second)
-			var addr net.Addr
-			addr, stunerr = getAddress(stunCtx, dialer, stunAddress)
-			if stunerr != nil {
-				log.Println("Can't get address", stunerr)
-			} else {
-				fmt.Println(addr)
-			}
-			stunCtxCancel()
-		}
 		select {
 		case <-myCtx.Done():
 			log.Println("Run connection context expired", myCtx.Err())
@@ -85,12 +125,109 @@ func runConnection(ctx context.Context, dialer *net.Dialer, webSocketEchoURL str
 			log.Println("Can't write to websocket", err)
 			return
 		}
+		select {
+		case writeTimeCh <- time.Now():
+		default:
+		}
+	}
+}
+
+type stunsHolder []string
+
+func (h *stunsHolder) getAddress(ctx context.Context, dialer *net.Dialer) (net.Addr, time.Time, error) {
+	n := len(*h)
+	if n == 0 {
+		log.Panicln("empty stuns")
+		return nil, time.Now(), io.ErrNoProgress
+	} else if n == 1 {
+		only := (*h)[0]
+		addr, err := getAddress(ctx, dialer, only)
+		t := time.Now()
+		return addr, t, err
+	}
+	var current atomic.Int32
+	const th = 2
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+	var addrs []net.Addr
+	bads := make(map[*string]struct{})
+	var lastTime time.Time
+	for range [th]int{} {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			var addr net.Addr
+			var err error = io.ErrNoProgress
+			var subLastTime time.Time
+			var subBads []*string
+			for err != nil {
+				i := int(current.Add(1) - 1)
+				if i >= n {
+					break
+				}
+				stunAddress := (*h)[i]
+				addr, err = getAddress(ctx, dialer, stunAddress)
+				if err != nil {
+					addr = nil
+					subBads = append(subBads, &(*h)[i])
+
+				} else {
+					subLastTime = time.Now()
+				}
+			}
+			mu.Lock()
+			defer mu.Unlock()
+			for _, v := range subBads {
+				bads[v] = struct{}{}
+			}
+			if err != nil {
+				return
+			}
+			lastTime = subLastTime
+			for _, a := range addrs {
+				if a.Network() == addr.Network() &&
+					a.String() == addr.String() {
+					return
+				}
+			}
+			addrs = append(addrs, addr)
+		}()
+	}
+	wg.Wait()
+	if len(bads) > 0 {
+		sort.Slice(*h, func(i, j int) bool {
+			_, bi := bads[&(*h)[i]]
+			_, bj := bads[&(*h)[j]]
+			if bi == bj {
+				return i < j
+			} else {
+				return bj
+			}
+		})
+	}
+	switch len(addrs) {
+	default:
+		{
+			var sb strings.Builder
+			sb.WriteString("too many results:")
+			for _, addr := range addrs {
+				sb.WriteString(" ")
+				sb.WriteString(addr.String())
+			}
+			return nil, lastTime, errors.New(sb.String())
+		}
+	case 0:
+		return nil, lastTime, errors.New("no results")
+	case 1:
+		return addrs[0], lastTime, nil
 	}
 }
 
 func getAddress(ctx context.Context, dialer *net.Dialer, stunAddress string) (addr net.Addr, err error) {
-	log.Println("Connecting to stun")
-	conn, err := dialer.DialContext(ctx, "tcp", stunAddress)
+	myCtx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+	log.Println("Connecting to stun", stunAddress)
+	conn, err := dialer.DialContext(myCtx, "tcp", stunAddress)
 	if err != nil {
 		log.Println("Can't connect stun", err)
 		return
@@ -102,7 +239,7 @@ func getAddress(ctx context.Context, dialer *net.Dialer, stunAddress string) (ad
 	}
 	defer client.Close()
 	message := stun.MustBuild(stun.TransactionID, stun.BindingRequest)
-	log.Println("Doing stun request")
+	log.Println("Doing stun request", stunAddress)
 	err2 := client.Do(message, func(res stun.Event) {
 		if res.Error != nil {
 			err = res.Error
